5572753db5bd07bf73ad61ab76739418ef2a41e4
Irina Mateush <ismateush@yandex-team.ru>
2020-11-03 22:58:18 +0300

    начало фильтрации

--- js/main.js
+++ js/main.js
@@ -5,13 +5,14 @@
   const map = document.querySelector(`.map`);
   const pinListElement = document.querySelector(`.map__pins`);
   const typeOfHousing = document.querySelector(`#housing-type`);
-  const PINS_COUNT = 8;
+  const PINS_COUNT = 5;
   let livingType = `any`;
 
   const addPinsToMap = (pins) => {
     const pinsFragment = document.createDocumentFragment();
 
     for (let i = 0; i < PINS_COUNT; i++) {
+      // console.log(pins[i]);
       pinsFragment.appendChild(window.map.renderPin(pins[i]));
     }
 
@@ -32,26 +33,34 @@
   };
 
   typeOfHousing.addEventListener(`change`, function () {
+    if (document.querySelector(`.popup`) !== null) {
+      window.map.closePopup();
+    }
+
     livingType = typeOfHousing.value;
     if (livingType !== `any`) {
       let samePins = window.pins.filter(function (pin) {
         return pin.offer.type === livingType;
       });
-      window.samePins = samePins;
+      successLoadHandler(samePins);
+    } else {
+      successLoadHandler(window.pins);
     }
   });
 
-  const updatePins = function (pins) {
-    addPinsToMap(pins);
-  };
-
   const successLoadHandler = (pins) => {
-    // addPinsToMap(pins);
-    updatePins(pins);
-    activationСard();
-
     const pinElements = document.querySelectorAll(`.map__pin:not(.map__pin--main)`);
     window.pins = pins;
+
+    if (pinElements !== null) {
+      for (let pinElement of pinElements) {
+        pinElement.classList.add(`hidden`);
+      }
+    }
+
+    addPinsToMap(pins);
+    activationСard();
+
     for (let pinElement of pinElements) {
       pinElement.addEventListener(`click`, function () {
         const pinsAvatar = pinElement.querySelector(`img`).getAttribute(`src`);
